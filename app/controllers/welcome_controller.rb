64b8608e93e50ef81288798fdfd10d9e002d26c4
Alfredo Segundo <alfredocavalcanti@gmail.com>
2014-09-03 15:08:31 -0300

    refactor - move logic to model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,6 @@ class WelcomeController < ApplicationController
 	skip_before_action :require_login, only: [:index]
 	
 	def index
-	  	first_day_month = Date.today.beginning_of_month
-		last_day_month = Date.today.end_of_month
-		@total_expenses = Expense.where(:budget_date => first_day_month..last_day_month).sum(:value)
+		@total_expenses = Expense.get_expenses_for(Date.today)
 	end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,13 @@
 class Expense < ActiveRecord::Base
   belongs_to :contributor
 
+  def self.get_expenses_for(budget_date)
+    first_day_month = budget_date.beginning_of_month
+    last_day_month = budget_date.end_of_month
+    Expense.where(:budget_date => first_day_month..last_day_month).sum(:value)
+  end
+  
   def formated_budget_date
-  	budget_date.strftime("%B %Y")
+    budget_date.strftime("%B %Y")
   end
 end
