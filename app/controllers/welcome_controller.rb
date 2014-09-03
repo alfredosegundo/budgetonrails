eca7a0da02f6217d18338846c6468489b5403c8c
Alfredo Segundo <alfredocavalcanti@gmail.com>
2014-09-03 00:28:22 -0300

    add budget date to expenses

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,8 @@ class WelcomeController < ApplicationController
 	skip_before_action :require_login, only: [:index]
 	
 	def index
+	  	first_day_month = Date.today.beginning_of_month
+		last_day_month = Date.today.end_of_month
+		@total_expenses = Expense.where(:budget_date => first_day_month..last_day_month).sum(:value)
 	end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,7 @@
 class Expense < ActiveRecord::Base
   belongs_to :contributor
+
+  def formated_budget_date
+  	budget_date.strftime("%B %Y")
+  end
 end
