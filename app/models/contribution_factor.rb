6ee095f52a05c50027c27fc1fa18735cff6a007d
Alfredo Segundo <alfredocavalcanti@gmail.com>
2015-02-17 12:03:16 -0300

    Add contribution factor tests

--- app/models/contribution_factor.rb
+++ app/models/contribution_factor.rb
@@ -1,5 +1,10 @@
 class ContributionFactor < ActiveRecord::Base
-	validate :same_month
+	validate :same_month, :max_factor_value
+
+	def self.find_for_month(date)
+	  	last_month_day = date.end_of_month
+		ContributionFactor.where("initial_date <= ?", last_month_day).order(initial_date: :desc).first
+	end
 
 	private
 	def same_month
@@ -7,6 +12,12 @@ class ContributionFactor < ActiveRecord::Base
 	  	last_month_day = self.initial_date.end_of_month
 	  	if ContributionFactor.where(:initial_date => first_month_day..last_month_day).any?
 	  		errors.add(:initial_date, "can't be two in the same month")
+	  	end
+	end
+
+	def max_factor_value
+		if self.factor > 100.0
+	  		errors.add(:factor, "max value is 100.")
 	  	end
 	end
 end
--- spec/models/contribution_factor_spec.rb
+++ spec/models/contribution_factor_spec.rb
@@ -2,11 +2,26 @@ require 'rails_helper'
 
 RSpec.describe ContributionFactor, :type => :model do
 
-	it "should have only one in a month" do
-		ContributionFactor.new(factor: 85.2, initial_date: Date.new(2015, 2, 1)).save
-		ContributionFactor.new(factor: 85.2, initial_date: Date.new(2015, 2, 12)).save
+	it "should be only one per month" do
+		ContributionFactor.new(factor: 1, initial_date: Date.new(2015, 2, 1)).save
+		ContributionFactor.new(factor: 1, initial_date: Date.new(2015, 2, 12)).save
 
 		expect(ContributionFactor.count).to eq(1)
+	end
+
+	it "biggest factor should be 100" do
+		ContributionFactor.new(factor: 101, initial_date: Date.new(2015, 2, 1)).save
+
+		expect(ContributionFactor.count).to eq(0)
+	end
+
+	it "shold be active until a new one is created" do
+		ContributionFactor.new(factor: 1, initial_date: Date.new(2014, 1, 1)).save
+		ContributionFactor.new(factor: 2, initial_date: Date.new(2015, 1, 1)).save
+
+		contributionFactor = ContributionFactor.find_for_month(Date.new(2015,2,1))
+
+		expect(contributionFactor.factor).to eq(2)
 	end
 
 end
