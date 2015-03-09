be22e8d20d8d96474c573c111b6e8570dcc22016
Alfredo Segundo <alfredocavalcanti@gmail.com>
2015-03-08 23:26:19 -0300

    [Card 13] update contribution factor

--- app/controllers/contribution_factors_controller.rb
+++ app/controllers/contribution_factors_controller.rb
@@ -21,7 +21,17 @@ class ContributionFactorsController < ApplicationController
     @factor = ContributionFactor.find(params[:id])
   end
 
+  def edit
+    @contribution_factor = ContributionFactor.find(params[:id])
+  end
+
   def update
+    @contribution_factor = ContributionFactor.find(params[:id])
+    if @contribution_factor.update(contribution_factor_params)
+      redirect_to @contribution_factor
+    else
+      render 'edit'
+    end
   end
 
   def destroy
