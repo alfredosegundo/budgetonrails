e3d9b8667b8b1c801f2b0cfadc59f932af90929f
Alfredo Segundo <alfredocavalcanti@gmail.com>
2015-04-05 04:17:49 -0300

    add entity validation for expense.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,16 +1,17 @@
 class Expense < ActiveRecord::Base
+  validates :description, :budget_date, :value, presence: true
   belongs_to :contributor
   before_save :set_buget_date_to_midnight
   has_one :realization
   has_one :expected_expense, through: :realization
 
-  def self.get_expenses_same_month_of(date)
+  def self.get_expenses_same_month_of date
     first_day_month = date.beginning_of_month
     last_day_month = date.end_of_month
     Expense.where(:budget_date => first_day_month..last_day_month)
   end
 
-  def self.get_expenses_sum_for(date)
+  def self.get_expenses_sum_for date
     get_expenses_same_month_of(date).sum(:value)
   end
   
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,26 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Expense, :type => :model do
-	context "#get_expenses_sum_for" do
-		budget_date = DateTime.new(2014,9,1)
-		it "should return expenses in budget month" do
-			expense_inside_budget_date = Expense.new(description: "any", value: 1.0, budget_date: budget_date)
-			expense_outside_budget_date = Expense.new(description: "any", value: 1.0, budget_date: DateTime.new(2014,1,1))
-			expense_inside_budget_date.save
-			expense_outside_budget_date.save
+  context "entity validation" do
+    it "budget_date is required" do
+      Expense.new.save
 
-			expense_for_budget_month = Expense.get_expenses_sum_for(budget_date)
+      expect(Expense.all.count).to eq 0
+    end
 
-			expect(expense_for_budget_month).to eq(1.0)
-		end
+    it "description is required" do
+      Expense.new(budget_date: DateTime.now).save
 
-		it "should sum all expenses in the same month" do
-			Expense.new(description: "any", value: 1.0, budget_date: DateTime.new(2014,9,2)).save
-			Expense.new(description: "any", value: 1.0, budget_date: DateTime.new(2014,9,3)).save
+      expect(Expense.all.count).to eq 0
+    end
 
-			expense_for_budget_month = Expense.get_expenses_sum_for(budget_date)
+    it "value is required" do
+      Expense.new(budget_date: DateTime.now, description: "some").save
 
-			expect(expense_for_budget_month).to eq(2.0)
-		end
-	end
+      expect(Expense.all.count).to eq 0
+    end
+  end
+  
+  context "#get_expenses_sum_for" do
+    budget_date = DateTime.new(2014,9,1)
+    it "should return expenses in budget month" do
+      expense_inside_budget_date = Expense.new(description: "any", value: 1.0, budget_date: budget_date)
+      expense_outside_budget_date = Expense.new(description: "any", value: 1.0, budget_date: DateTime.new(2014,1,1))
+      expense_inside_budget_date.save
+      expense_outside_budget_date.save
+
+      expense_for_budget_month = Expense.get_expenses_sum_for(budget_date)
+
+      expect(expense_for_budget_month).to eq(1.0)
+    end
+
+    it "should sum all expenses in the same month" do
+      Expense.new(description: "any", value: 1.0, budget_date: DateTime.new(2014,9,2)).save
+      Expense.new(description: "any", value: 1.0, budget_date: DateTime.new(2014,9,3)).save
+
+      expense_for_budget_month = Expense.get_expenses_sum_for(budget_date)
+
+      expect(expense_for_budget_month).to eq(2.0)
+    end
+  end
 end
