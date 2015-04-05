63947b10b43f3e3f3123e0c75ff04c4926851d37
Alfredo Segundo <alfredocavalcanti@gmail.com>
2015-04-05 04:20:37 -0300

    [Card 18] tests for finding not realized expected expenses

--- app/models/expected_expense.rb
+++ app/models/expected_expense.rb
@@ -1,4 +1,10 @@
 class ExpectedExpense < ActiveRecord::Base
   has_many :realizations
   has_many :expenses, through: :realizations
+
+  def self.not_realized_on date
+    initial_date = date.beginning_of_month
+    final_date = date.end_of_month
+    ExpectedExpense.where("NOT expenses.budget_date between ? AND ? OR expenses IS NULL", initial_date, final_date).eager_load(:expenses)
+  end
 end
---
+++ spec/models/expected_expense_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe ExpectedExpense, :type => :model do  
+  context "#not_realized_on" do
+    budget_date = DateTime.new(2014,9,1)
+
+    it "should return an empty array if there is no expected_expenses" do
+      result = ExpectedExpense.not_realized_on budget_date
+
+      expect(result.size).to eq 0
+    end
+
+    it "should return expected_expenses if there is a not realized one" do
+      ExpectedExpense.new(description: "some", value: 10).save
+
+      result = ExpectedExpense.not_realized_on budget_date
+
+      expect(result.to_a.size).to eq 1
+    end
+
+    it "should return only not realized expenses" do
+      ee = ExpectedExpense.new(description: "some", value: 10)
+      ee.save
+      ee.expenses << Expense.new(value: 10, description: "some", budget_date: budget_date)
+
+      result = ExpectedExpense.not_realized_on budget_date
+
+      expect(result.size).to eq 0
+    end
+  end
+end
