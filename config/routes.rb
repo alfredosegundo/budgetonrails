3047f6fa0825d0eec2220055e672024445c1317e
Alfredo Segundo <alfredocavalcanti@gmail.com>
2015-04-05 04:34:01 -0300

    remove legacy code

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'periodic_expenses/index'
-
-  get 'periodic_expenses/new'
-
-  get 'periodic_expenses/edit'
-
-  get 'periodic_expenses/destroy'
-
   get 'welcome/index'
   get "/auth/google_oauth2/callback" => "sessions#create"
   get "log_out" => "sessions#destroy", :as => "log_out"
@@ -15,6 +7,7 @@ Rails.application.routes.draw do
   get 'welcome/home' => 'welcome#home', :as => 'home'
 
   post 'welcome/home' => 'welcome#change_date'
+
   resources :contributors do
   	resources :contributions
   end
