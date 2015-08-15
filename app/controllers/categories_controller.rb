fc343d76549a0659c69fe04fd24fa2143d0e7268
Alfredo Segundo <alfredocavalcanti@gmail.com>
2015-08-15 18:22:27 -0300

    Card-27 CRUD for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,47 @@
 class CategoriesController < ApplicationController
   def index
-  	@categories = Category.all
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new color: "#73defc"
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path
   end
 
   def show
+    @category = Category.find params[:id]
   end
+
+  private
+    def category_params
+      params.require(:category).permit(:color, :name)
+    end
 end
