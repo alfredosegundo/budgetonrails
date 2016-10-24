a2ae3e652e4facb92faf088c4b6c7f44ab7f4970
Alfredo Segundo <alfredocavalcanti@gmail.com>
2016-10-24 16:53:17 -0300

    Added /lib folder to auto load.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module Budgetonrails
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(
-        #{config.root}/app/presenters
+        #{config.root}/app/presenters,
+        #{config.root}/lib
     )
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
